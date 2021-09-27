6497277fd2c697b12f29195088862b0b0c4eccfd
Joseph-K-B <303josephb@gmail.com>
2021-09-27 16:50:15 -0700

    fetch function working, red test, data not seeding

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,12 +3,33 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
-describe('demo routes', () => {
-  beforeEach(() => {
+describe('trivia application', () => {
+  beforeEach(() => 
+  {
     return setup(pool);
   });
 
-  afterAll(() => {
+  it('Get\'s a random trivia question from API', () =>
+  {
+    return request(app)
+      .get('/api/random')
+      .then((res) => 
+      {
+        expect(res.body).toEqual(
+          [{
+            category: expect.any(String),
+            type: expect.any(String),
+            difficulty: expect.any(String),
+            question: expect.any(String),
+            correct_answer: expect.any(String),
+            incorrect_answers: expect.any(String)
+          }]
+        );
+      });
+  });
+
+  afterAll(() => 
+  {
     pool.end();
   });
 });
