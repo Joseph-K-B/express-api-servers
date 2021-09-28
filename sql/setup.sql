96c8bc61f3741872f57c3e103dab405d91f4f4e1
Joseph-K-B <303josephb@gmail.com>
2021-09-27 18:08:09 -0700

    data mapping properly, not inserting into table

--- sql/setup.sql
+++ sql/setup.sql
@@ -3,15 +3,15 @@ DROP TABLE IF EXISTS randomtrivia;
 CREATE TABLE randomtrivia (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     category VARCHAR(512) NOT NULL,
-    answer_type VARCHAR(256),
+    -- answer_type VARCHAR(256),
     difficulty VARCHAR(256) NOT NULL,
     question VARCHAR(512) NOT NULL,
-    correct_answer VARCHAR(512) NOT NULL,
-    incorrect_answer VARCHAR(512) NOT NULL
+    answer VARCHAR(512) NOT NULL
+    -- incorrect_answer VARCHAR(512) NOT NULL
 );
 
--- INSERT INTO randomTrivia (category, difficulty, question, correct_answer)
---     VALUES ('category one', 'difficulty one', 'question one', 'answer one')
+INSERT INTO randomTrivia (category, difficulty, question, answer)
+    VALUES ('category one', 'difficulty one', 'question one', 'answer one')
 
 -- DROP TABLE IF EXISTS randomTrivia;
 
