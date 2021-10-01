37ca873e6971cf919521be530b31a938de38c2a4
Joseph-K-B <303josephb@gmail.com>
2021-09-30 18:55:51 -0700

    getId green

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,14 +2,15 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
-// import randomTriviaService from '../lib/services/randomTriviaService.js';
 import randomQuestions from '../lib/models/randomTrivia.js';
+// import randomTriviaService from '../lib/services/randomTriviaService.js';
+// import randomQuestions from '../lib/models/randomTrivia.js';
 
 describe('trivia application', () => {
-  // beforeEach(() => 
-  // {
-  //   return setup(pool);
-  // });
+  beforeEach(() => 
+  {
+    return setup(pool);
+  });
 
   it('Get\'s random trivia questions from API', async() =>
   {
@@ -28,7 +29,6 @@ describe('trivia application', () => {
       .post('/api/random')
       .send(
         {
-          id: '1',
           category: 'Geography',
           difficulty: 'easy',
           question: 'What city is built on two continents?',
@@ -55,13 +55,21 @@ describe('trivia application', () => {
 
   it('gets random question by id from db', async() =>
   {
+    const question = await randomQuestions.insert(
+      {
+        category: 'Astronomy',
+        difficulty: 'easy',
+        question: 'How many moons does Earth have?',
+        answer: '1'
+      }
+    );
     return request(app)
-      .get('/api/random/2')
+      .get('/api/random/1')
       .then(res =>
       {
         // console.log('Test File get:id', res.body);
         expect(res.body).toEqual({
-          id: expect.any(String),
+          id: '1',
           category: expect.any(String),
           difficulty: expect.any(String),
           question: expect.any(String),
@@ -72,15 +80,12 @@ describe('trivia application', () => {
 
   it('updates the question by the id', async () =>
   {
-    // const putQuestion = await randomQuestions.insert({ answer: 'Not Istanbul' });
-
     return request(app)
       .put('/api/random/1')
       .send({ answer: 'Not Istanbul' })
       .then((res) =>
       {
         expect(res.body).toEqual({
-          id: '1',
           category: 'Geography',
           difficulty: 'easy',
           question: 'What city is built on two continents?',
