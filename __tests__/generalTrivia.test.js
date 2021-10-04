48b02328a2300c3a2314fe1f02b5b04e470ab644
Joseph-K-B <303josephb@gmail.com>
2021-10-03 20:03:45 -0700

    get by id passing for general

--- __tests__/generalTrivia.test.js
+++ __tests__/generalTrivia.test.js
@@ -1,8 +1,11 @@
+/* eslint-disable no-unused-vars */
 import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 import generalQuestions from '../lib/models/generalTrivia.js';
+import generalTrivia from '../lib/models/generalTrivia.js';
+import { generalFetch } from '../lib/utils/fetch.js';
 
 
 
@@ -42,6 +45,45 @@ describe('trivia application', () => {
       {
         console.log('TESTING POST', res.body);
         expect(res.body).toEqual(expect.any(Object));
+      });
+  });
+
+
+
+  it('gets by id', async () =>
+  {
+    const question1 = await generalTrivia.insert(
+      {
+        category: 'General Knowledge',
+        difficulty: 'easy',
+        question: 'Color of the sky?',
+        answer: 'Green'
+      });
+    const question2 = await generalTrivia.insert(
+      {
+        category: 'General Knowledge',
+        difficulty: 'easy',
+        question: 'Leader of the free world?',
+        answer: 'JFK'
+      });
+    const question3 = await generalTrivia.insert({
+      category: 'General Knowledge',
+      difficulty: 'easy',
+      question: 'Opposite of up?',
+      answer: 'down'
+    });
+
+    return request(app)
+      .get('/api/general/2')
+      .then(res =>
+      {
+        expect(res.body).toEqual({
+          id: '2',
+          category: 'General Knowledge',
+          difficulty: expect.any(String),
+          question: expect.any(String),
+          answer: expect.any(String)
+        });
       });
   });
 
